4eca1564d314bdeb05a787f7b1ff0a8025aba931
DinukaNimantha <n.dinuka@gtngroup.com>
2023-03-02 12:09:53 +0530

    Added an exception to Java code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,5 +2,14 @@ package org.example;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            count += i;
+        }
+
+        System.out.println("Count is " + count);
+
+        int div = 14/0;
     }
 }
